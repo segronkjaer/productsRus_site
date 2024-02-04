fd7afb5c766440f0dd4ae598d147091e4e90e0ca
sara gronkjaer <sagr0001@stud.kea.dk>
2024-02-04 16:20:32 +0100

    js

---
+++ js/product.js
@@ -0,0 +1,9 @@
+// https://kea-alt-del.dk/t7/api/products/1525 //
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+    .then(response=>response.json())
+    .then(data=>showProduct(data))
+
+    function showProduct(product){
+        console.log(product);
+
+    }
