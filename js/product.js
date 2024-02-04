1ea7aacf815f2f62d0b9f2a05f1987f05a1985f0
sara gronkjaer <sagr0001@stud.kea.dk>
2024-02-04 16:40:52 +0100

    produkt

--- js/product.js
+++ js/product.js
@@ -5,5 +5,41 @@ fetch("https://kea-alt-del.dk/t7/api/products/1525")
 
     function showProduct(product){
         console.log(product);
-
+        document.querySelector(".grid_produkt h1").textContent = productdisplayname;
+        document.querySelector("img").src='https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp';
+        document.querySelector(".grid_produkt h3").textContent = brandname;
+        document.querySelector(".color p").textContent = colour1;
+        document.querySelector(".price p").textContent = price;
     }
+
+    // status: det vises ikke i browseren og den skriver fejl i teminalen //
+
+/*
+    id": 1525,
+    "gender": "Unisex",
+    "category": "Accessories",
+    "subcategory": "Bags",
+    "articletype": "Backpacks",
+    "basecolour": "Navy Blue",
+    "season": "Fall",
+    "productionyear": 2010,
+    "usagetype": "Casual",
+    "productdisplayname": "Deck Navy Blue Backpack",
+    "parsed": 1,
+    "soldout": 0,
+    "relid": 1525,
+    "price": 1299,
+    "discount": 55,
+    "variantname": "Deck Backpack",
+    "brandname": "Puma",
+    "brandbio": "PUMA is the World's Fastest Sports Brand",
+    "brandimage": "http://assets.myntassets.com/assets/images/2015/11/17/11447736932686-113016-3ff8sf.jpg",
+    "agegroup": "Adults-Unisex",
+    "colour1": "NA",
+    "colour2": "NA",
+    "fashiontype": "Fashion",
+    "materialcaredesc": null,
+    "sizefitdesc": null,
+    "description": "<p>asfafaf<br> kasjhdkashd</p>",
+    "styledesc": null
+*/
