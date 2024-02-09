38747ba23a08ee5ed162bdbff1bd251832f44508
sara gronkjaer <sagr0001@stud.kea.dk>
2024-02-09 12:02:38 +0100

    en del ændringer

--- js/product.js
+++ js/product.js
@@ -1,21 +1,23 @@
-// https://kea-alt-del.dk/t7/api/products/1525 
+// https://kea-alt-del.dk/t7/api/products/1525
 
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
 fetch("https://kea-alt-del.dk/t7/api/products/" + id)
-    .then((response) => response.json())
-    .then((data) => showProduct(data));
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
 
-    function showProduct(product){
-        console.log(product);
-        document.querySelector(".grid_produkt h1").textContent = product.productdisplayname;
-        document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/${product.id}.webp`;
-        document.querySelector(".grid_produkt h3").textContent = product.brandname;
-        document.querySelector(".grid_produkt p span").textContent = product.colour1;
-        document.querySelector(".grid_produkt h4 span").textContent = product.price;
-    }
-
+function showProduct(product) {
+  console.log("Her kommer et produkt", product);
+  document.querySelector(".grid_product h1").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector(".grid_product h3").textContent = product.brandname;
+  document.querySelector(".grid_product p span").textContent = product.colour1;
+  document.querySelector(".grid_product h4 span").textContent = product.price;
+}
 
 /*
     "id": 1525,
--- js/productlist.js
+++ js/productlist.js
@@ -1,44 +1,46 @@
 /* test */
 const urlParams = new URLSearchParams(window.location.search);
-    const category = urlParams.get("kategory");
+const category = urlParams.get("category");
 
-const url = "https://kea-alt-del.dk/t7/api/products?category="+category;
+const url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
 
-    fetch(url)
-        .then((response) => response.json())
-        .then(dataRecieved);
+fetch(url)
+  .then((response) => response.json())
+  .then(dataRecieved);
 
-    function dataRecieved(data) {
-        // we have the data
-        console.log(data);
-        data.forEach(showProduct);
-            
-        };
-        
-        function showProduct(product){
-            console.log(product);
-            // fang template
-            const template = document.querySelector("template").content;
-            // man kopierer templaten
-            const myClone = template.cloneNode(true); 
-            // ændrer indholdet 
-            myClone.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-            myClone.querySelector(".productdisplayname").textContent = product.productdisplayname;
-            myClone.querySelector(".brandname").textContent = product.brandname;
-            myClone.querySelector(".price").textContent = `${product.price},-`;
-            // hvis produktet er udsolgt 
-             if(product.soldout){
-                myClone.querySelector("article").classList.add("produkt_udsolgt");
-            } //virker ikke
-            
-            myClone.querySelector(".link").setAttribute("href", `product.html?id=${product.id}`); // virker ikke
+function dataRecieved(data) {
+  // we have the data
+  console.log(data);
+  data.forEach(showProduct);
+}
 
-            // appende
-            const parentElement = document.querySelector("main");
-            parentElement.appendChild(myClone);
-        }
+function showProduct(product) {
+  console.log(product);
+  // fang template
+  const template = document.querySelector("template").content;
+  // man kopierer templaten
+  const myClone = template.cloneNode(true);
+  // ændrer indholdet
+  myClone.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  myClone.querySelector(".productdisplayname").textContent =
+    product.productdisplayname;
+  myClone.querySelector(".brandname").textContent = product.brandname;
+  myClone.querySelector(".price").textContent = `${product.price},-`;
+  // hvis productet er udsolgt
+  if (product.soldout) {
+    myClone.querySelector("article").classList.add("product_udsolgt");
+  } //virker ikke
 
+  myClone
+    .querySelector("p.link a")
+    .setAttribute("href", `product.html?id=${product.id}`); // virker ikke
 
+  // appende
+  const parentElement = document.querySelector("main");
+  parentElement.appendChild(myClone);
+}
 
 /* min HTML struktur: 
 <article class="smallProduct">
@@ -46,7 +48,7 @@ const url = "https://kea-alt-del.dk/t7/api/products?category="+category;
             <h2>Sahara Team India Fanwear Round Neck Jersey</h2>
             <p class="brand">Nike</p>
             <p class="price">DKK 895,-</p>
-            <p class="link"><a href="produkt.html">LÆS MERE</a></p>
+            <p class="link"><a href="product.html">LÆS MERE</a></p>
         </article> 
 */
 
